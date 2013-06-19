48af1d357ccda3cebe3b3c8d5bf412c055686976
Danny Casady <dpcasady@gmail.com>
2013-06-18 20:09:26 -0500

    update plugin descriptor

--- CompassSassGrailsPlugin.groovy
+++ CompassSassGrailsPlugin.groovy
@@ -10,18 +10,17 @@ class CompassSassGrailsPlugin {
             'src/stylesheets/**/*'
     ]
 
-    def author = "Stefan Kendall"
-    def authorEmail = "stefankendall@gmail.com"
     def title = "Compass Framework - SASS/SCSS support"
-    def description = '''
-Compass, SASS and SCSS support for Grails. Automatically compiles .scss/.sass during run-app, and adds other framework functionality.
-'''
-
+    def author = "Danny Casady"
+    def authorEmail = "dpcasady@gmail.com"
+    def description = "Compass, SASS and SCSS support for Grails. Automatically compiles .scss/.sass during run-app, and adds other framework functionality."
     def documentation = "http://grails.org/plugin/compass-sass"
+    def issueManagement = [ system: "GitHub", url: "https://github.com/dpcasady/compass-sass/issues" ]
+    def scm = [url: "https://github.com/dpcasady/compass-sass"]
 
     def license = "APACHE"
     def developers = [
-            [name:'Joel Rosenberg']
+            [name:'Joel Rosenberg'],
+            [name:'Stefan Kendall']
     ]
-    def scm = [url: "https://github.com/stefankendall/compass-sass"]
 }
